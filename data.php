a7c4c0e854ff40a565a5a79b0d4ecc27ee19f309
Dokakoka <Dokakoka>
2024-05-06 17:44:43 +0300

    fixing image size

---
+++ data.php
@@ -0,0 +1,12 @@
+Array ( 
+    [title] => Music festival 
+    [date] => Array ( [start] => Array ( [date] => 05/09/2024 [hour] => 8 [minutes] => 0 [ampm] => AM ) [end] => Array ( [date] => 05/09/2024 [hour] => 6 [minutes] => 0 [ampm] => PM ) 
+    [repeat] => Array ( [type] => daily [interval] => 1 [advanced] => [end] => never [end_at_date] => [end_at_occurrences] => 10 ) ) 
+    [exceptionsdays] => Array ( [start] => Array ( [hour] => 8 [minutes] => 0 [ampm] => AM ) [end] => Array ( [hour] => 6 [minutes] => 0 [ampm] => PM ) ) [in_days] => Array ( [:i:] => :val: ) 
+    [excerpt] => [content] => bhasjdasdjasbjasjbhdbhj 
+    [featured_image] => https://rootrivercurrent.org/wp-content/uploads/2024/05/audience-1853662_640.jpg 
+    [cost] => 50$ for a single ticket [cost_auto_calculate] => 0 
+    [fields] => Array ( [1] => https://www.facebook.com/egyptianmuseum2015 [2] => https://www.facebook.com/egyptianmuseum2015 ) 
+    [location_id] => 0 
+    [location] => Array ( [name] => Egyptian museum [address] => The Egyptian Museum in Cairo [opening_hour] => Cairo [zipcode] => 11795 [latitude] => 30.048446691679402 [longitude] => 31.233667396053672 )
+    [organizer_id] => 0 [organizer] => Array ( [name] => Ahmed Mohamed Abdel Hamied Morsy [email] => ahmedmorsi3634@gmail.com [tel] => 01017479880 ) [banner] => Array ( [display] => 0 [color] => #000000 [use_featured_image] => 0 [image] => ) [read_more] => [more_info] => https://www.facebook.com/egyptianmuseum2015 [post_id] => -1 )
